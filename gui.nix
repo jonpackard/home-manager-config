8e9317d8f6ee1a2407dba056a6212ad4c9a20a33
Jonathan Packard <jwpackard@duck.com>
2023-10-17 11:43:25 -0400

    Cleanup

--- gui.nix
+++ gui.nix
@@ -23,24 +23,8 @@
       pkgs.anydesk
       pkgs.discord
       pkgs.jellyfin-mpv-shim
-      #pkgs.fortune
-      #pkgs.neofetch
       pkgs.vlc
       pkgs.lighthouse-steamvr # Power management for SteamVR lighthouses
       unstable.prismlauncher
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
-
 }
